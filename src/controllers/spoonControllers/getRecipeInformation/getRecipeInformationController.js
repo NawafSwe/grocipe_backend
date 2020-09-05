f22c1d0301cf780178d1261244db7be324a5f752
Nawaf <Nawaf.jr9@hotmial.com>
2020-09-05 11:06:53 +0300

    heroku push

--- src/controllers/spoonControllers/getRecipeInformation/getRecipeInformationController.js
+++ src/controllers/spoonControllers/getRecipeInformation/getRecipeInformationController.js
@@ -1,18 +1,18 @@
 /* ----------------- importing packages and files ---------------- */
 const axios = require('axios'),
-    prepareRequest = require('./prepareRequest'),
-    transformer = require('./transformer');
+	prepareRequest = require('./prepareRequest'),
+	transformer = require('./transformer');
 
 const getRecipeInformationRequest = async (recipe) => {
-    try { 
-        const request = prepareRequest(recipe);
-        const response = await axios.get(request);
-        const result = response.data;
-        //const standardResult = transformer(result);
-        return result;
-    } catch (e) {
-        console.log('error occurred in spoon getRecipeInformation controller', e);
-     }
-}
- 
+	try {
+		const request = prepareRequest(recipe);
+		const response = await axios.get(request);
+		const result = response.data;
+		const standardResult = transformer(result);
+		return result;
+	} catch (e) {
+		console.log('error occurred in spoon getRecipeInformation controller', e);
+	}
+};
+
 module.exports = getRecipeInformationRequest;
