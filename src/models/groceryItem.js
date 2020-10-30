fad5cd9251c3ceff0d6c6fc4c77a17abccedccd8
NawafSwe <nawaf.jr9@hotmail.com>
2020-10-31 01:45:52 +0300

    refactore

--- src/models/groceryItem.js
+++ src/models/groceryItem.js
@@ -4,15 +4,14 @@ const mongoose = require('mongoose');
 /*---------------------------- creating schema ----------------------------*/
 
 const groceryItemSchema = mongoose.Schema({
-	title: { type: String, require: true },
+	title: { type: String, required: true },
 	price: { type: Number },
 	description: { type: String },
-	ingredientName: { type: String, require: true },
+	ingredientName: { type: String, required: true },
 	images: { type: String },
-	category: { type: String },
+	category: { type: String }
 });
 
 /*---------------------------- creating the model in the DB ----------------------------*/
 const GroceryItem = mongoose.model('GroceryItem', groceryItemSchema);
-
 module.exports = GroceryItem;
